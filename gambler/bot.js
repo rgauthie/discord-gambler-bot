8120f2ef86f3042c1b67ab2aa0e9d87d271aaa85
Reed Gauthier <rgauthie@stedwards.edu>
2020-04-01 21:05:04 -0500

    no message

--- gambler/bot.js
+++ gambler/bot.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 function checkMulti() {
     res = getResFromFile('multiRolls.txt');
     console.log(res.length);
+    console.log(res);
     if (res.length > 2) {
          return true;
     } else {
